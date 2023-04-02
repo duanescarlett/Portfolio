6fc4b696a82c752da707a7a77159a6dfbc149ac2
Duane Scarlett <duanescarlett@gmail.com>
2023-04-02 10:28:13 -0500

    created bio

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+// import { PermanentMarker } from '@next/font/google'
+
+// const permanent = PermanentMarker({
+//   subset: ['latin'],
+//   weight: ['400', '700']
+  
+// })
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{html,js,jsx,ts,tsx}",
+    "./components/**/*.{js,ts,tsx,jsx,html}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
